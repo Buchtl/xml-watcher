eaab0470f75efd68e541f07eda51e7fa36696fb0
Chris <schauer@inf-schauer.de>
2025-07-31 16:23:22 +0200

    tests

---
+++ tests/test_xml_utils.py
@@ -0,0 +1,24 @@
+# test_math_utils.py
+import unittest
+from pathlib import Path
+from src import xml_utils
+
+BASE_DIR = Path(__file__).parent
+FIXTURE_DIR = BASE_DIR / "fixtures"
+
+
+class TextXmlWatcher(unittest.TestCase):
+
+    def test_get_parts_amount(self):
+        expected = 2
+        actual = len(xml_utils.find_parts(FIXTURE_DIR / "sample.xml"))
+        self.assertEqual(actual, expected)
+
+    def test_get_parts_content(self):
+        actual_parts = xml_utils.find_parts(FIXTURE_DIR / "sample.xml")
+        self.assertEqual(actual_parts[0].body, "This is a test.".encode("utf-8"))
+        self.assertEqual(actual_parts[1].body, "This is a test too.".encode("utf-8"))
+
+
+if __name__ == "__main__":
+    unittest.main()
